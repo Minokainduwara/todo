f89597704640ee0413b7ebfddc4d62e4da5dcb43
Minokainduwara <minokainduwara@gmail.com>
2025-08-17 11:55:56 +0530

    UI

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 export default function Todolist() {
+  let todo = [
+    "Go to the gym",
+    "Buy groceries",
+    "Read a book",
+  ]
   return (
-    <>
-    <div>Todolist</div>
-    <div>test</div>
-    </>
+    <ul className='main'>
+      {todo.map((todo,todoIndex) => {
+        return(
+          <li className= 'todoItem' key={todoIndex}>{todo}</li>
+        )
+      })}
+
+    </ul>
     
   )
 }
